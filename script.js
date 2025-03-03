3a68657f4067a7412ce41b85e79127b2f9280e51
AnweshaBhatt475 <143898640+AnweshaBhatt475@users.noreply.github.com>
2025-03-03 23:23:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,31 +1,30 @@
-class Person {
-  constructor(name, age) {
-    this.name = name;
-    this.age = age;
-  }
-
-  greet() {
-    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
-  }
+function Car(make, model) {
+  this.make = make;
+  this.model = model;
 }
 
-class Employee extends Person {
-  constructor(name, age, jobTitle) {
-    super(name, age); // Calling the parent class constructor
-    this.jobTitle = jobTitle;
-  }
-
-  jobGreet() {
-    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
-  }
+Car.prototype.getMakeModel = function() {
+  return `${this.make} ${this.model}`;
 }
 
-// Do not change code below this line
-window.Person = Person;
-window.Employee = Employee;
+function SportsCar(make, model, topSpeed) {
+  Car.call(this, make, model); // Call the Car constructor
+  this.topSpeed = topSpeed;
+}
+
+// Set up inheritance from Car
+SportsCar.prototype = Object.create(Car.prototype);
+SportsCar.prototype.constructor = SportsCar;
+
+SportsCar.prototype.getTopSpeed = function() {
+  return this.topSpeed;
+}
+
+// Do not change the code below
+window.Car = Car;
+window.SportsCar = SportsCar;
 
 // Example usage
-let person = new Person("Alice", 25);
-person.greet(); 
-let employee = new Employee("Bob", 30, "Manager");
-employee.jobGreet(); 
+const car = new SportsCar("Ferrari", "Testarossa", 200);
+console.log(car.getMakeModel()); // Output: Ferrari Testarossa
+console.log(car.getTopSpeed()); // Output: 200
