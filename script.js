2bb8cfc58f731700a971e9e2132de21d8538bdb4
AnweshaBhatt475 <143898640+AnweshaBhatt475@users.noreply.github.com>
2025-03-03 23:19:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
-// Complete the js code
-function Car(make, model) {}
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
 
-function SportsCar(make, model, topSpeed) {}
+  greet() {
+    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+  }
+}
 
-// Do not change the code below
-window.Car = Car;
-window.SportsCar = SportsCar;
+class Employee extends Person {
+  constructor(name, age, jobTitle) {
+    super(name, age); // Calling the parent class constructor
+    this.jobTitle = jobTitle;
+  }
+
+  jobGreet() {
+    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
+  }
+}
+
+// Do not change code below this line
+window.Person = Person;
+window.Employee = Employee;
+
+// Example usage
+let person = new Person("Alice", 25);
+person.greet(); 
+let employee = new Employee("Bob", 30, "Manager");
+employee.jobGreet(); 
